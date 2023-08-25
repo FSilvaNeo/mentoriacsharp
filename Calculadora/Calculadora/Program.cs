97b43a83ff91a964da2f33a013923d4a0c13053b
fasilvaneo <alves_felipesilva@outlook.com>
2023-08-24 22:13:50 -0300

    add new projects: Calculadora and SomaMedia

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao;
+            bool controle = true;
+            float n1;
+            float n2;
+            float resultado;
+
+            while (controle)
+            {
+                opcao = MostrarMenu();
+
+                switch (opcao)
+                {
+                    case "0":
+                        controle = false;
+                        break;
+
+                    case "1":
+                        n1 = LerNumero("primeiro");
+                        n2 = LerNumero("segundo");
+                        resultado = Somar(n1, n2);
+                        ExibirResultado("soma", resultado);
+                        break;
+
+                    case "2":
+                        n1 = LerNumero("primeiro");
+                        n2 = LerNumero("segundo");
+                        resultado = Subtrair(n1, n2);
+                        ExibirResultado("subtração", resultado);
+                        break;
+
+                    case "3":
+                        n1 = LerNumero("primeiro");
+                        n2 = LerNumero("segundo");
+                        resultado = Multiplicar(n1, n2);
+                        ExibirResultado("multiplicação", resultado);
+                        break;
+
+                    case "4":
+                        n1 = LerNumero("primeiro");
+                        n2 = LerNumero("segundo");
+                        resultado = Dividir(n1, n2);
+                        ExibirResultado("divisão", resultado);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção invalida!");
+                        break;
+                }
+
+                Console.Clear();
+            }
+        }
+
+        static string MostrarMenu()
+        {
+            Console.WriteLine("\tCALCULADORA\n");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("3 - Multiplicar");
+            Console.WriteLine("4 - Dividir");
+            Console.WriteLine("0 - Sair");
+            return Console.ReadLine();
+        }
+
+        static float Somar(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        static float Subtrair(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+
+        static float Multiplicar(float n1, float n2)
+        {
+            return n1 * n2;
+        }
+
+        static float Dividir(float n1, float n2)
+        {
+            return n1 / n2;
+        }
+
+        static float LerNumero(string ordinal)
+        {
+            Console.WriteLine("Entre com o " + ordinal + " número:");
+            return float.Parse(Console.ReadLine());
+        }
+
+        static void ExibirResultado(string operacao, float resultado)
+        {
+            Console.WriteLine("O resultado da " + operacao + " é: " + resultado);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SomaMedia/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var quantityNumbers = ShowMenu();
+
+            var numbers = ReadNumbers(quantityNumbers);
+            var sum = numbers.Select(x => x).Sum();
+            var average = numbers.Select(x => x).Average();
+
+            ShowResult(sum, average);
+        }
+
+        static int ShowMenu()
+        {
+            int MIN = 3;
+            int MAX = 10;
+            int quantityNumbers;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\tSOMA E MÉDIA\n");
+                Console.WriteLine("Informe a quantidade de números serão informados para o cálculo:");
+                Console.WriteLine("A quantidade de números deve estar entre {0} e {1}", MIN, MAX);
+                int.TryParse(Console.ReadLine(), out quantityNumbers);
+            }
+            while (quantityNumbers < MIN || quantityNumbers > MAX);
+            return quantityNumbers;
+        }
+
+        static List<int> ReadNumbers(int quantityNumbers)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < quantityNumbers; i++)
+            {
+                int.TryParse(Console.ReadLine(), out int number);
+                numbers.Add(number);
+            }
+            return numbers;
+        }
+
+        static void ShowResult(int sum, double average)
+        {
+            Console.WriteLine("A soma dos números é: {0}", sum);
+            Console.WriteLine("A média dos números é: {0}", average);
+            Console.ReadKey();
+        }
+    }
+}
